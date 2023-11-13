200c67135899a4410ec0717b7756305cc246a905
Aravinth-S-731 <aravinth7871867225@gmail.com>
2023-11-13 20:12:28 +0530

    Document Addition | .acive link

--- App.js
+++ App.js
@@ -5,3 +5,20 @@ function openMenu(){
 function closeMenu(){
     navLinks.style.right = "-200px";
 }
+
+function dropdown() {
+    document.getElementById("dropDowncontent").classList.toggle("show");
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropDownbtn')) {
+        var dropdowns = document.getElementsByClassName("dropDowncontent");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
